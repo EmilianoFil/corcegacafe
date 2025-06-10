44adb698f0ef28e28c9ab177e4f4962927b47cbe
EmilianoFil <emilianofilgueira@gmail.com>
2025-06-10 11:13:49 -0300

    Actualizo upload.js y .gitignore, limpieza final

--- functions/upload.js
+++ functions/upload.js
@@ -1,7 +1,6 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const fetch = require("node-fetch");
-
 const db = admin.firestore();
 
 exports.uploadMenuToGitHub = functions.https.onCall(async (data, context) => {
