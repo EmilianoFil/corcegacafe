3e3dc15e1ade2d17db808f86dbe5444f50a10400
EmilianoFil <emilianofilgueira@gmail.com>
2025-06-10 10:01:12 -0300

    agrego js de rol

---
+++ functions/asignarRol.js
@@ -0,0 +1,16 @@
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+const uid = "RZIzXw3gsGSmEHf3oG7jxpWh9RF3"; // reemplazá por el UID real
+const rol = "dueño"; // o 'empleado', etc.
+
+admin.auth().setCustomUserClaims(uid, { rol })
+  .then(() => {
+    console.log(`✅ Se asignó el rol "${rol}" al usuario ${uid}`);
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error("❌ Error al asignar el rol:", error);
+    process.exit(1);
+  });
